5339dd48b070548b62afd95a5f16e4c36a836fb5
Valmyr Tavares <valmyrtavares71@gmail.com>
2021-07-30 16:32:01 -0300

    renderInfo done

--- script.js
+++ script.js
@@ -13,24 +13,35 @@ let warnig= "";
 let playing = false;
 
 
-//Events 
+reset();
 document.querySelector('.reset').addEventListener('click', reset) //Colocando um evento no botão reset
 
-//Function
+
 function reset() {
     warning = "";
     let random = Math.floor(Math.random() * 2) //Criando um número aleatório entre 2 1 e 0
     player = (random===0) ? 'x': 'o'; 
-//Manobra para limpar o meu square
-//Lembrando que aqui o square não é um array. Talvez por isso seja for e não foreach
-//Ele diz que eu posso acessar tanto square.a1 quanto square['a1']
+
     for (let i in square){
         square[i] = ''; 
     }
     playing = true;
-    //iniciar o jogo
-
-    //Vamos disparar duas outra funções
+   
     renderSquare();
     renderInfo();
+}
+
+function renderSquare(){
+    //Teste para ver se está rolando
+    // for(let i in square){
+    //     console.log("item ", i)
+    // }
+
+    for(let i in square){
+        let item = document.querySelector(`idv[data-item]=${i}`)//lembrando que aqui o i é a1, a2, a3...
+        if(square[i] !== ''){
+            item.innerHTML = SQUARE[i];
+        }//Aqui o square[i] corresponde ao que está dentro de cada quadrado e se ele tiver algo dentro ele vai colocar dentro de novo
+        //não é muito intuitivo não...
+    }
 }
