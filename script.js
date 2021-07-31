49d84ca72aea7ba8916701c0151dc3137bcbf4d4
Valmyr Tavares <valmyrtavares71@gmail.com>
2021-07-31 12:03:22 -0300

    isFull done

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-//dados iniciais
 
-
-//Todos os 9 quadros do jogo da velha
 let square = {
     a1:'', a2:"", a3:"",
     b1:'', b2:"", b3:"",
@@ -19,27 +16,21 @@ document.querySelectorAll('.item').forEach(item => {
     item.addEventListener('click', itemClick)
 })
 
-function itemClick(event){
-    
-    //console.log(event.target) Ele printa a div que foi clicada
+function itemClick(event){    
     
     let item = event.target.getAttribute('data-item');
-    //console.log("Clicou em " + item);
-    if(square[item]===''){
+  
+    if(playing && square[item]===''){
         square[item]=player;
       renderSquare();
       togglePlayer()
     }
 }
 
-
-function reset() {   
-    
+function reset() {       
     warning = "";
-    let random = Math.floor(Math.random() * 2) //Criando um número aleatório entre 2 1 e 0
-    player = (random===0) ? 'x': 'o'; 
-
-    
+    let random = Math.floor(Math.random() * 2) 
+    player = (random===0) ? 'x': 'o';     
     
     for (let i in square){
         square[i] = ''; 
@@ -51,32 +42,28 @@ function reset() {
     renderInfo();
 }
 
-function renderSquare(){
-   // Teste para ver se está rolando
-    // for(let i in square){
-    //     console.log("item ", i)
-    // }
 
-    for(let i in square){
-      
+function renderSquare(){ 
+
+    for(let i in square){      
             let item = document.querySelector(`div[data-item=${i}]`)//lembrando que aqui o i é a1, a2, a3...
             item.innerHTML = square[i];
-        }//Aqui o square[i] corresponde ao que está dentro de cada quadrado e se ele tiver algo dentro ele vai colocar dentro de novo
-        //não é muito intuitivo não...
-
-        checkWinnerFor();//Troquei a função por checkWinner só para ir direto para a função sem passar pela checkGame
-    
+        }
+        checkGame();    
 }
+
 
 function renderInfo(){
     document.querySelector('.vez').innerHTML = player;
     document.querySelector('.resultado').innerHTML = warning;
 }
 
+
 function togglePlayer(){
    player =  (player==='x' )? 'o':'x';
    renderInfo();
 }
+
 
 function checkGame(){
     if(checkWinnerFor('x')){
@@ -91,7 +78,7 @@ function checkGame(){
     }
 }
 
-function checkWinnerFor(){
+function checkWinnerFor(player){
     let pos = [
         'a1,a2,a3',
         'b1,b2,b3',
@@ -103,24 +90,7 @@ function checkWinnerFor(){
 
         'a1,b2,c3',
         'c1,b2,a3',        
-    ];  
-    // Primeira escrita mais simplificada
-
-    // for(let w in pos){
-    //     let pArray = pos[w].split(',');
-    //     pArray.every((option)=>{
-    //         console.log(option)                      
-    //        if(square[option]=== player){
-    //            return true
-    //        }else{
-    //            return false
-    //        }
-    //     }) 
-    // }
-    
-    /* Lembrando que aqui console.log(square[option]) retorna 8 vezes vazia
-    E console.log(option) retorna o primeiro item de cada array do vencedor a1,b1,c1,a2...
-    */
+    ];      
 
     for(let w in pos){
         let pArray = pos[w].split(',');
@@ -130,12 +100,14 @@ function checkWinnerFor(){
         }
     }
    return false;
-
 }
-/* E aqui se a função com somente o que tem que ter ela pode retornar false ou true, se uma das opções
-retornar true ela preenche o hasWon com true e entra no return true e para a execução, se passar em todas e
-nada retornar true sai do for e retorna false. Enfatizando que nesse step não tem teste
-*/
-function isFull(){
 
+
+function isFull(){
+    for(let i in square){
+        if(square[i]=== ''){
+            return false;
+        }
+    }
+    return true;
 }
